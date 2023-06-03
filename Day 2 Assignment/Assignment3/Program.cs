b69ffabdf56ece8698802a3a9add310a3761b8b6
R1dZ2 <110802736+R1dZ2@users.noreply.github.com>
2023-06-03 13:39:29 +0400

    Add files via upload

---
+++ Day 2 Assignment/Assignment3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Claim claim1 = new Claim(001, 01, 2);
+            Claim claim2 = new Claim(002, 01, 5);
+
+            Assistant assistant = new Assistant();
+            assistant.ApproveClaim("UnderWriter",ref claim1);
+            assistant.ApproveClaim("InsuranceManager", ref claim2);
+            Console.WriteLine(claim1.CommentUW);
+            Console.WriteLine(claim1.CommentBM);
+            Console.WriteLine(claim2.CommentUW);
+            Console.WriteLine(claim2.CommentBM);
+        }
+    }
+}
