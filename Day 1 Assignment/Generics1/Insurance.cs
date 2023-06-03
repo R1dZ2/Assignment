e44bc05784d9d6198caf37393814a4023bbc38e1
R1dZ2 <110802736+R1dZ2@users.noreply.github.com>
2023-06-03 14:12:51 +0400

    Add files via upload

---
+++ Day 1 Assignment/Generics1/Insurance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Insurance
+    {
+        public string InsuranceId { get; set; }
+        public string InsuranceName { get; set; }
+        public double InsurancePolicyAmount { get; set; }
+        public double InsurancePremium { get; set; }
+        public int InsuranceTerm { get; set; }
+
+        public Insurance()
+        {
+
+        }
+
+        public Insurance(string insuranceId, string insuranceName, int insuranceTerm, double amount, double premium)
+        {
+            InsuranceId = insuranceId;
+            InsuranceName = insuranceName;
+            InsuranceTerm = insuranceTerm;
+            InsurancePolicyAmount = amount;
+            InsurancePremium = premium;
+        }
+    }
+}
---
+++ Day 1 Assignment/Generics1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agent agent = new Agent();
+
+            Insurance insuranceOne = new Insurance("I001","GFA",1,2000,4000);
+            Insurance insuranceTwo =new Insurance("I002", "Phoenix", 1, 4000, 8000);
+            Insurance insuranceThree = new Insurance("I001", "GFA", 1, 2900, 4000);
+            Insurance insuranceFour = new Insurance("I003", "SWAN", 2, 3500, 6000);
+
+            agent.AddPolicy(insuranceOne);
+            agent.AddPolicy(insuranceTwo);
+            agent.AddPolicy(insuranceThree);
+            agent.Display();
+
+            agent.SearchPolicy("I001");
+
+            agent.AddPolicy(insuranceFour);
+            agent.Display();
+
+            agent.DeletePolicy("I003");
+            agent.Display();
+        }
+    }
+}
