6af76804461b16456d5be3868fa42cdc51525d17
R1dZ2 <110802736+R1dZ2@users.noreply.github.com>
2023-06-03 14:18:23 +0400

    Add files via upload

---
+++ Day 2 Assignment/Delegates 3/Calculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public class Calculation
+    {
+
+        Calculator calculator = new Calculator();
+
+        public int PerformCalculation(int choice, int numberOne, int numberTwo)
+        {
+            CalculatorDelegate calculatorDelegate;
+            switch (choice)
+            {
+                case 1:
+                    calculatorDelegate = new CalculatorDelegate(calculator.Add);
+                    break;
+
+                case 2:
+                    calculatorDelegate = new CalculatorDelegate(calculator.Subtract);
+                    break;
+
+                case 3:
+                    calculatorDelegate = new CalculatorDelegate(calculator.Multiply);
+                    break;
+
+                default:
+                    calculatorDelegate = new CalculatorDelegate(calculator.Add);
+                    break;
+            }
+            return calculatorDelegate(numberOne, numberTwo);
+        }
+    }
+}
---
+++ Day 2 Assignment/Delegates 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+            var result = calculation.PerformCalculation(1, 2, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
